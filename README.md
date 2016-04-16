ef962d3d3c19608af86a880701c8a3e42e2641ee
Nicolas Hillegeer <nicolas@hillegeer.com>
2016-04-16 23:56:14 +0200

    findcycles: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# findcycles
-findcycles filters directed DOT graphs, returning only the nodes that are in a cycle.
+findcycles(1)
+=============
+
+findcycles(1) filters a DOT format graph, keeping only nodes that are in in
+cycles. Input is taken on stdin, output is provied on stdout.
+
+It proved useful to me while debugging cycles in systemd units. For systemd
+graphs, only the After edges are relevant for ordering cycles. You can
+filter them out with grep(1) or some other tool to get a more concise output
+graph.
+
+To generate a suitable graph from systemd, you can issue a shell command as
+described in the following link: http://unix.stackexchange.com/a/227963.
+
+Afterwards, I did the following to study the cycles:
+
+```bash
+$ <remount-cycle.dot grep -P 'digraph|\}|green' | findcycles |
+    dot -Tsvg > cycles-order.svg
+$ <remount-cycle.dot findcycles |
+    dot -Tsvg > cycles-all.svg
+```
+
+NOTE: findcycles(1) may very well contain bugs when used outside the realm
+of systemd graphs, you have been warned
---
+++ findcycles.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/awalterschulze/gographviz"
+	"github.com/awalterschulze/gographviz/parser"
+)
+
+func main() {
+	// Create an Abstract Syntax Tree (AST) from a DOT representation.
+	ast, err := parser.Parse(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Render the AST into a graph.
+	g := gographviz.NewGraph()
+	gographviz.Analyse(ast, g)
+
+	// For each node, find the cycles it participates in. Keep all nodes
+	// that participate in cylces in r.
+	r := map[string]bool{}
+	for node := range g.Nodes.Lookup {
+		union(r, cycles(node, g.Edges.SrcToDsts))
+	}
+
+	// To render a graph consisting only of cycles:
+	//
+	// 1. Create a new graph.
+	// 2. Loop over all edges int he input graph.
+	// 3. Add an edge to the new graph if both the source and the
+	//    destination are in a cycle.
+	fg := gographviz.NewGraph()
+	fg.SetName(g.Name)
+	fg.SetDir(true)
+	for _, edge := range g.Edges.Edges {
+		if r[edge.Src] && r[edge.Dst] {
+			fg.AddNode("", edge.Src, nil)
+			fg.AddNode("", edge.Dst, nil)
+			fg.AddEdge(edge.Src, edge.Dst, true, edge.Attrs.Copy())
+		}
+	}
+
+	io.WriteString(os.Stdout, fg.String())
+}
+
+// Cycles returns a set of all nodes that are involved in cycles starting in
+// start. It is horribly inefficient, both from an implementation as well as
+// an algorithmic point of view. It uses DFS to find cycles. I didn't feel
+// like implementing Johnson's algorithm. If you want that, Python has a
+// library called networkx, which has a simple_cycles() function that uses
+// an efficient algorithm.
+func cycles(start string, adj map[string]map[string]*gographviz.Edge) map[string]bool {
+	return dfs(start, start, nil, adj)
+}
+
+// dfs returns all nodes that are in a simple cycle starting in start.
+func dfs(start, cur string, stack []string, adj map[string]map[string]*gographviz.Edge) map[string]bool {
+	// Check for cycle or cross.
+	if in(stack, cur) {
+		if cur == start {
+			return set(stack) // Found a cycle, return the current stack as a set.
+		}
+		return nil // Found a cross, just return.
+	}
+
+	r := map[string]bool{}
+	childStack := append(stack, cur)
+	// Loop over all possible destinations of cur.
+	for dst := range adj[cur] {
+		// Add all nodes that are in a cycle as found by the recursive call
+		// to dfs.
+		union(r, dfs(start, dst, childStack, adj))
+	}
+	return r
+}
+
+// Check if elem is in stack.
+func in(stack []string, elem string) bool {
+	for _, s := range stack {
+		if s == elem {
+			return true
+		}
+	}
+	return false
+}
+
+// Adds b's elements to a, in-place
+func union(a, b map[string]bool) {
+	for k, v := range b {
+		if v {
+			a[k] = v
+		}
+	}
+}
+
+// Turn a list into a set.
+func set(a []string) map[string]bool {
+	r := map[string]bool{}
+	for _, k := range a {
+		r[k] = true
+	}
+	return r
+}
